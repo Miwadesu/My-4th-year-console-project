2828ff4bf377fef6c0ff244daa6e8ef01be2f72a
Miwadesu <nyragail.dl@gmail.com>
2026-07-01 16:47:00 +0800

    Update main.c
    Initial upload

--- main.c
+++ main.c
@@ -1,13 +1,4 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2761
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
-\margl1440\margr1440\vieww28600\viewh18000\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
-
-\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 #include <stdio.h>\
+#include <stdio.h>\
 \
 void buyItem();\
 void playGame(char name[]);\
